eeffd5383174528d26d1c5ced888ca665c836a15
Feoktistova Daria <feoktistova.dariafeok@yandex.ru>
2022-06-29 12:55:31 +0300

    Доработан тест "Добавление группы"

--- test_add_group.py
+++ test_add_group.py
@@ -12,20 +12,14 @@ class TestAddGroup(unittest.TestCase):
     
     def test_add_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_groups_page(wd)
         self.create_group(wd, Group(name="qw", header="wq"))
-        self.return_to_groups_page(wd)
         self.logout(wd)
 
     def test_add_empty_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_groups_page(wd)
         self.create_group(wd, Group(name="", header=""))
-        self.return_to_groups_page(wd)
         self.logout(wd)
 
     def logout(self, wd):
@@ -37,6 +31,7 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_link_text("group page").click()
 
     def create_group(self, wd, group):
+        self.open_groups_page(wd)
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
@@ -48,13 +43,14 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("group_header").send_keys(group.header)
         # submit group creation
         wd.find_element_by_name("submit").click()
+        self.return_to_groups_page(wd)
 
     def open_groups_page(self, wd):
         # open group page
         wd.find_element_by_link_text("groups").click()
 
     def login(self, wd, username, password):
-        # login
+        self.open_home_page(wd)
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
