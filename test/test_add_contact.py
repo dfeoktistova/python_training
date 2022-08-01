b0b0e35dfbdad79f8203061f0178c418b0aa73be
Feoktistova Daria <feoktistova.dariafeok@yandex.ru>
2022-08-01 16:29:49 +0300

    Созданы классы-помощники, реализован перенос вспомогательных функций в эти классы для тестов добавления групп и контактов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -12,23 +12,23 @@ def app(request):
 
 def test_add_contact(app):
     app.open_home_page()
-    app.login(username="admin", password="secret")
-    app.open_contacts_page()
-    app.fill_contact_form(Contact(firstname="Иван", middlename="Иванов", lastname="Иванович", nickname="Ivanov_Ivan", title="q", company="w", address="e", home="555-55-55",
-                                           mobile="8-921-666-66-66", work_number="911", fax="666", email="1a@ya.ru", email2="2b@mail.ru", email3="3c@rambler.com",
-                                           homepage="https://ru.wikipedia.org/wiki/", bday="1", bmonth="January", byear="1980", aday="1", amonth="January", ayear="1990",
-                                           address2="qwerty", phone2="555", notes="qwerty1"))
+    app.session.login(username="admin", password="secret")
+    app.contact.open_contacts_page()
+    app.contact.fill_form(Contact(firstname="Иван", middlename="Иванов", lastname="Иванович", nickname="Ivanov_Ivan", title="q", company="w", address="e", home="555-55-55",
+                                  mobile="8-921-666-66-66", work_number="911", fax="666", email="1a@ya.ru", email2="2b@mail.ru", email3="3c@rambler.com",
+                                  homepage="https://ru.wikipedia.org/wiki/", bday="1", bmonth="January", byear="1980", aday="1", amonth="January", ayear="1990",
+                                  address2="qwerty", phone2="555", notes="qwerty1"))
     app.return_to_home_page()
-    app.logout()
+    app.session.logout()
 
 def test_add_empty_contact(app):
     app.open_home_page()
-    app.login(username="admin", password="secret")
-    app.open_contacts_page()
-    app.fill_contact_form(Contact(firstname="", middlename="", lastname="", nickname="", title="", company="", address="", home="",
-                                           mobile="", work_number="", fax="", email="", email2="", email3="",
-                                           homepage="", bday="", bmonth="-", byear="", aday="", amonth="-", ayear="",
-                                           address2="", phone2="", notes=""))
+    app.session.login(username="admin", password="secret")
+    app.contact.open_contacts_page()
+    app.contact.fill_form(Contact(firstname="", middlename="", lastname="", nickname="", title="", company="", address="", home="",
+                                  mobile="", work_number="", fax="", email="", email2="", email3="",
+                                  homepage="", bday="", bmonth="-", byear="", aday="", amonth="-", ayear="",
+                                  address2="", phone2="", notes=""))
     app.return_to_home_page()
-    app.logout()
+    app.session.logout()
 
