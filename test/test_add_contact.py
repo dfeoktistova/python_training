84a1620929ad47046f74149bc044c2e2610ffeaf
Feoktistova Daria <feoktistova.dariafeok@yandex.ru>
2022-08-02 19:30:08 +0300

    Оптимизация ввода данных в формы

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -2,10 +2,8 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-    app.open_home_page()
     app.session.login(username="admin", password="secret")
-    app.contact.open_add_contacts_page()
-    app.contact.fill_form(
+    app.contact.create(
         Contact(firstname="Иван", middlename="Иванов", lastname="Иванович", nickname="Ivanov_Ivan", title="q",
                 company="w", address="e", home="555-55-55",
                 mobile="8-921-666-66-66", work_number="911", fax="666", email="1a@ya.ru", email2="2b@mail.ru",
@@ -13,18 +11,14 @@ def test_add_contact(app):
                 homepage="https://ru.wikipedia.org/wiki/", bday="1", bmonth="January", byear="1980", aday="1",
                 amonth="January", ayear="1990",
                 address2="qwerty", phone2="555", notes="qwerty1"))
-    app.return_to_home_page()
     app.session.logout()
 
 
 def test_add_empty_contact(app):
-    app.open_home_page()
     app.session.login(username="admin", password="secret")
-    app.contact.open_add_contacts_page()
-    app.contact.fill_form(
+    app.contact.create(
         Contact(firstname="", middlename="", lastname="", nickname="", title="", company="", address="", home="",
                 mobile="", work_number="", fax="", email="", email2="", email3="",
                 homepage="", bday="", bmonth="-", byear="", aday="", amonth="-", ayear="",
                 address2="", phone2="", notes=""))
-    app.return_to_home_page()
     app.session.logout()
--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -1,5 +1,4 @@
 def test_del_contact(app):
     app.session.login(username="admin", password="secret")
-    app.contact.open_contact_page()
     app.contact.delete_first_contact()
     app.session.logout()
