85eafa1f3459e8a7174c2cdc35c4c0e3129871cc
Feoktistova Daria <feoktistova.dariafeok@yandex.ru>
2022-08-11 15:47:14 +0300

    Загружена информация для групп в виде списков

--- model/group.py
+++ model/group.py
@@ -1,4 +1,5 @@
 class Group:
-    def __init__(self, name=None, header=None):
+    def __init__(self, name=None, header=None, id=None):
         self.name = name
         self.header = header
+        self.id = id
