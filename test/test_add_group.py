7593855c1babdc64978bb3dbfcdc856e2ee054b5
Feoktistova Daria <feoktistova.dariafeok@yandex.ru>
2022-08-13 01:22:08 +0300

    Сортировка списков перед сравнением для тестов добавления и модификации групп

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,13 +3,19 @@ from model.group import Group
 
 def test_add_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group(name="qw", header="wq"))
+    group = Group(name="qw", header="wq")
+    app.group.create(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 def test_add_empty_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group(name="", header=""))
+    group = Group(name="", header="")
+    app.group.create(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,17 +2,17 @@ from model.group import Group
 
 
 def test_modify_group_name(app):
-    if app.group.count() == 0:
-        app.group.create(Group(name="Test_group"))
     old_groups = app.group.get_group_list()
-    app.group.modify_first_group(Group(name="Test_group1"))
+    group = Group(name="Test_group1")
+    group.id = old_groups[0].id
+    app.group.modify_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 def test_modify_group_header(app):
-    if app.group.count() == 0:
-        app.group.create(Group(name="Test_group"))
     old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(header="Test_group1"))
     new_groups = app.group.get_group_list()
