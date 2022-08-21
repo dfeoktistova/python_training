ee1fd37268ad16f0fa24499aa9bf14b518ad1ed7
Feoktistova Daria <feoktistova.dariafeok@yandex.ru>
2022-08-22 00:16:02 +0300

    Выполнен весь урок 7 для тестов групп

--- fixture/group.py
+++ fixture/group.py
@@ -45,13 +45,26 @@ class GroupHelper:
         self.return_to_groups_page()
         self.group_cache = None
 
+    def delete_group_by_id(self, id):
+        wd = self.app.wd
+        self.open_groups_page()
+        self.select_group_by_id(id)
+        # submit deletion
+        wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
+        self.group_cache = None
+
     def select_first_group(self):
         wd = self.app.wd
         wd.find_element_by_name("selected[]").click()
 
     def select_group_by_index(self, index):
         wd = self.app.wd
-        wd.find_elements_by_name("selected[]")[index].click()
+        wd.find_elements_by_name("input[value='%s']" % id).click()
+
+    def select_group_by_id(self, id):
+        wd = self.app.wd
+        wd.find_element_by_css_selector("selected[]")[id].click()
 
     def modify_first_group(self):
         self.modify_group_by_index(0)
--- test/test_del_group.py
+++ test/test_del_group.py
@@ -1,16 +1,21 @@
 # -*- coding: utf-8 -*-
+import random
+
 from model.group import Group
-from random import randrange
+import random
 
 
-def test_delete_some_group(app):
-    if app.group.count() == 0:
+def test_delete_some_group(app, db, check_ui):
+    if len(db.get_group_list()) == 0:
         app.group.create(Group(name="Test_group"))
-    old_groups = app.group.get_group_list()
-    index = randrange(len(old_groups))
-    app.group.delete_group_by_index(index)
-    new_groups = app.group.get_group_list()
+    old_groups = db.get_group_list()
+    group = random.choice(old_groups)
+    app.group.delete_group_by_id(group.id)
+    new_groups = db.get_group_list()
     assert len(old_groups) - 1 == len(new_groups)
     # удаляем из старого списка все элементы с 0 по 1 (правая граница не включается, т.е только элемент 0)
-    old_groups[index:index+1] = []
+    old_groups.remove(group)
     assert old_groups == new_groups
+    if check_ui:
+        assert sorted(map(clean, new_groups), key=Group.id_or_max) == sorted(app.group.get_group_list(),
+                                                                            key=Group.id_or_max)
