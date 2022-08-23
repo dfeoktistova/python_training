46ada625cc0c7c74a0f7ba125ba90d3a8c4f97d4
Feoktistova Daria <feoktistova.dariafeok@yandex.ru>
2022-08-23 16:46:29 +0300

    fix 21

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -11,5 +11,4 @@ def test_add_group(app, db, json_groups, check_ui):
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
     if check_ui:
-        assert sorted(map(clean, new_groups), key=Group.id_or_max) == sorted(app.group.get_group_list(),
-                                                                             key=Group.id_or_max)
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
